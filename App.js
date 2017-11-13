8b0355f057933aab84db309b0f5634733d95fd50
Sam <h.e._pennypacker@hotmail.com>
2017-11-13 15:19:54 -0700

    First Commit Mother Fucker

---
+++ App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import DeviceList from './DeviceList';
+import SettingsScreen from './SettingsScreen';
+
+class HomeScreen extends React.Component {
+
+  static navigationOptions = {
+    header: null
+  }
+
+  navigatePress = () => {
+    this.props.navigation.navigate('Settings')
+  }
+
+  render() {
+
+    return (
+      <View style={styles.container}>
+        <View style={{ height: 70, backgroundColor: 'steelblue'}}>
+          <Text style={styles.text}>Aumi</Text>
+        </View>
+        <DeviceList navigatePress={this.navigatePress}/>
+      </View>
+    );
+  }
+}
+
+const SimpleApp = StackNavigator({
+  Home: { screen: HomeScreen },
+  Settings: { screen: SettingsScreen }
+});
+
+export default class App extends React.Component {
+  render() {
+    return <SimpleApp />;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  text: {
+    paddingVertical: 10,
+    fontSize: 50,
+    textAlign: 'center',
+    color: 'white',
+  }
+});
+
+/*
+<View style={styles.container}>
+<View style={{ height: 70, backgroundColor: 'steelblue'}}>
+  <Text style={styles.text}>Aumi</Text>
+</View>
+<DeviceList/>
+</View>
+*/
---
+++ SettingsScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Alert, StyleSheet, ScrollView, Image, Text, View, TouchableHighlight } from 'react-native';
+
+export default class SettingsScreen extends React.Component {
+  static navigationOptions = {
+    title: 'AUMI Settings',
+  };
+  render() {
+    return (
+      <View>
+          
+      </View>
+    );
+  }
+}
+  
